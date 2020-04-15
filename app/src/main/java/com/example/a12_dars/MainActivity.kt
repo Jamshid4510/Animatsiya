437acd5806347d69b1d517075b0cac7b085c0787
Jamshid4510 <jqayumov97@gmail.com>
2020-04-15 18:16:28 +0500

    xml ni qo'shmayabdi

---
+++ app/src/main/java/com/example/a12_dars/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.a12_dars
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+//        alpha.setOnClickListener {
+//            val animation: Animation = AnimationUtils.loadAnimation(this, R.anim.alpha)
+//            rasm.startAnimation(animation)
+//        }
+
+
+    }
+}
